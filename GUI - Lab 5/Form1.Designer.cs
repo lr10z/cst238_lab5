85fb33eac2dafec2877cdd063959dbb0632b2065
Leander Rodriguez <lr10@me.com>
2015-03-27 16:48:50 -0700

    Initial commit

---
+++ GUI - Lab 5/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace GUI___Lab_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "++ LIFE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 381);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(460, 46);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(270, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(376, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(309, 157);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(686, 428);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ GUI - Lab 5/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI___Lab_5
+{
+    public partial class Form1 : Form
+    {
+        private Random numberGenerator;
+        public Form1()
+        {
+            InitializeComponent();
+            numberGenerator = new Random();
+            button1.Visible = false;
+        }
+        
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //update drawing on the form
+            Graphics g = e.Graphics;
+
+            Font font1 = new Font("Arial", 10, GraphicsUnit.Point);
+            g.DrawString("Pens", font1, Brushes.Black, new Point(1, 2));
+            Pen pen = new Pen(Color.LightGray, 1);
+            g.DrawLine(pen, 0, 20, 702, 20);
+            pen = new Pen(Color.Gray, 2);
+            g.DrawLine(pen, 0, 25, 702, 25);
+            pen = new Pen(Color.Black, 3);
+            g.DrawLine(pen, 0, 30, 702, 30);
+
+            // Solid Brush
+            font1 = new Font("Arial", 8, GraphicsUnit.Point);
+            g.DrawString("Solid Brush", font1, Brushes.Black, new Point(1, 40));
+            Brush brush = new SolidBrush(Color.BurlyWood);
+            g.FillRectangle(brush, 0, 55, 351, 55);
+
+            // Texture Brush
+            font1 = new Font("Arial", 18, GraphicsUnit.Point);
+            g.DrawString("Texture Brush", font1, Brushes.Black, new Point(1, 160));
+            brush = new TextureBrush(Properties.Resources.Desert);
+            g.FillRectangle(brush, 0, 200, 702, 466);
+            
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //These will create immediately drawn circles in the PictureBox
+            Graphics g = pictureBox1.CreateGraphics();
+
+            int x, y, r;
+            x = numberGenerator.Next(0, pictureBox1.Width);
+            y = numberGenerator.Next(0, pictureBox1.Height);
+            r = numberGenerator.Next(0, 50);
+            if (x + r >= pictureBox1.Width)
+                x = pictureBox1.Width - r;
+            if (y + r >= pictureBox1.Height)
+                y = pictureBox1.Height - r;
+            Rectangle rect = new Rectangle(x, y, r, r);
+            g.DrawImage(Properties.Resources.A10963, rect);
+        }
+        
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Color randomColor = Color.FromArgb(numberGenerator.Next(255), numberGenerator.Next(255),
+                                               numberGenerator.Next(255));
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(randomColor, 10);
+            g.DrawEllipse(pen, 50, 10, 100, 100);
+
+
+            Rectangle rect = new Rectangle(0, 0, panel1.Width, panel1.Height);
+            randomColor = Color.FromArgb(numberGenerator.Next(255), numberGenerator.Next(255),
+                                               numberGenerator.Next(255));
+            pen = new Pen(randomColor, 10);
+            g.DrawRectangle(pen, rect);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            
+        }
+
+    }
+}
